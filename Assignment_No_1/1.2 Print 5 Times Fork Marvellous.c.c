c4137ac44a9a5e5a191b5cfdf38d21be76565c55
Rohit-shedage <rohitshedage60@gmail.com>
2021-03-15 23:35:59 +0530

    Using_Loop_Print_5_Times_Name_Program

---
+++ Assignment_No_1/1.2 Print 5 Times Fork Marvellous.c.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void Display()
+{
+      int cnt = 0;
+
+      for(cnt = 1;cnt <= 5;cnt++)
+      {
+             printf("\n Marvellous\n");
+      }
+}
+
+int main()
+{
+        Display();
+        return 0;
+}
