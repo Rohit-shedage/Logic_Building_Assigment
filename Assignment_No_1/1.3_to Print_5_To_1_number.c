dd03d701661942c5be2ffb34e43ce70aac846d69
Rohit-shedage <rohitshedage60@gmail.com>
2021-03-16 23:22:22 +0530

    Print_5_To_1_Numbers_Using_Loop

---
+++ Assignment_No_1/1.3_to Print_5_To_1_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void Display()
+{
+	int i = 1;
+
+	while(i <= 5)
+	{
+		  printf("%d",i);
+		  i--;
+	}
+	return;
+}
+
+
+int main()
+{
+	 Display();
+
+	 return 0;
+}
