88f4ab5cc0c1407e83ff6542cd7843842b9b34fc
Rohit-shedage <rohitshedage60@gmail.com>
2021-04-10 22:03:17 +0530

    Find_Even_Factorial_Of_Given_Number

---
+++ Assignment_No_9/9.3.Program to find even factorial of given number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int  EvenFactorial(int iNo)
+{
+       int iRet = 1,Fact = 0;
+       if(iNo < 0)
+       {
+              iNo = -iNo;
+       }
+        for(Fact = 1;iNo > 0;iNo--)
+       {
+           Fact = Fact * iNo;
+           if(iNo%2 == 0)
+           {
+                 iRet = iRet * iNo;
+           }
+       }
+       return iRet;
+}
+int main()
+{
+      int iValue = 0,iRet  = 0;
+
+      printf("\n Enter a Number =>");
+      scanf("%d",&iValue);
+
+      iRet =  EvenFactorial(iValue);
+
+      printf("\n Even Factorial of number is  => %d.",iRet);
+
+      getch();
+      return 0;
+}
