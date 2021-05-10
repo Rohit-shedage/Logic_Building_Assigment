f78df2a580dfdfe0a3480f03f49b69345dc425e2
Rohit-shedage <ankitashedage2661@gmail.com>
2021-05-10 12:35:50 +0530

    Accept_Number_From_User_And_Return_The_Count_Of_Digit_In_Between_3_And_7

---
+++ Assignment_No_13/13.3.Accept number from user and return the count of digits in between 3 and 7.c
@@ -0,0 +1,42 @@
+/*
+3.Write a program which accept number from user and return the count of digits in between 3 and 7.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int   CountRange(int iNo)
+{
+      int Dig = 0,iCnt = 0;
+
+      if(iNo < 0)
+      {
+            iNo = -iNo;
+      }
+
+      while( iNo > 0)
+      {
+            Dig = iNo%10;
+            if(Dig > 3 && Dig < 7)
+            {
+                  iCnt++;
+            }
+            iNo /= 10;
+      }
+      return iCnt;
+}
+int main()
+{
+       int iValue = 0,iRet = 0;
+
+       printf("Enter number to find  Count in Between 3 and 7 That Number =>");
+       scanf("%d",&iValue);
+
+       iRet =  CountRange(iValue);
+       printf("\n\n --------------------------------------------------------------- \n");
+
+       printf("\n Count Between 3 and 7 are => %d.",iRet);
+
+       printf("\n\n --------------------------------------------------------------- \n");
+
+       return 0;
+}
