7ed8b5b26af1b7d90d644de1ac56147583880712
Rohit-shedage <rohitshedage60@gmail.com>
2021-06-09 12:37:13 +0530

    Accept_Number_Of_Rows_And_Columns_From_User_And_Display_Below_Pattern

---
+++ Assignment_No_16/16.1. Accept number of rows and number of columns from user and display below pattern.c
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// 1. Accept number of rows and number of columns from user and display below pattern.
+/// Input :  iRow = 4  iCol = 4
+/// Output : A B C D
+///              A B C D
+///              A B C D
+///              A B C D
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iRow,int iCol)
+{
+            int i = 0,j = 0;
+            char ch = 'A';
+
+            printf("\n ============================================== \n");
+
+            for(i = 1;i <= iRow;i++)
+            {
+                    ch = 'A';
+                  for(j = 1;j <= iCol;j++,ch++)
+                  {
+                         printf(" %c",ch);
+                  }
+                  printf("\n");
+            }
+
+            printf("\n ============================================== \n");
+
+            return;
+}
+
+
+
+int main()
+{
+         int iValue1 = 0, iValue2 = 0;
+
+         printf("\n Enter number of rows and columns =>");
+         scanf("%d %d",&iValue1, &iValue2);
+
+        Pattern(iValue1, iValue2);
+        return 0;
+}
