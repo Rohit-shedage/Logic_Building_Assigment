a3b7932f4eda77b318e4786166651653a6e056c1
Rohit-shedage <rohitshedage60@gmail.com>
2021-07-25 19:09:13 +0530

    Accept_N_Number_From_User_And_Display_All_Such_Elements_Which_Are_Even_And_Divisible_By_5

---
+++ Assignment_No_21/21.3. Accept N numbers from user and display all such elements which are even and divisible by 5.c
@@ -0,0 +1,74 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 3. Accept N numbers from user and display all such elements which are even and divisible by 5. */
+///    Input :  N :   6
+///  Elements : 85 66 3 80 93 88
+///  Output : 80
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display(int Arr[], int iLength)
+{
+          int i = 0;
+
+          printf("\n\n ==================Number Even & Divisible By 5 in Array======================= \n");
+
+
+          for(i = 0;i < iLength;i++)
+          {
+                   if(Arr[i]%2 == 0)
+                   {
+                           if(Arr[i]%5 == 0)
+                           {
+                                    printf("\n Number Even and Divisible By 5 are => %d.",Arr[i]);
+                           }
+                  }
+          }
+
+           printf("\n\n ======================================================================== \n");
+
+
+          return;
+}
+
+int main()
+ {
+              int iSize = 0,iCnt = 0;
+              int *p = NULL;
+
+              printf("\n Enter number of elements =>");
+              scanf("%d",&iSize);
+
+               p = (int *)malloc(iSize * sizeof(int));
+
+               if(p == NULL)
+               {
+                               printf(" \n Unable to allocate memory");
+                                return -1;
+                }
+
+                printf("\n\n ==========================Displaying Array================================ \n");
+
+                for(iCnt = 0;iCnt < iSize;iCnt++)
+                {
+                             printf("\n Given Element %d is => %d.",iCnt+1,p[iCnt]);
+                }
+
+                 printf("\n\n =========================Accepting Array================================== \n");
+
+                for(iCnt = 0;iCnt<iSize; iCnt++)
+                {
+                               printf("\n Enter element %d => ",iCnt+1);
+                               scanf("%d",&p[iCnt]);
+                }
+
+
+                 Display(p, iSize);
+
+
+               free(p);
+
+              return 0;
+}
+
+
